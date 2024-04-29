14b53284fe9909cb4d2bafb479fff71c50ca1f38
Avi0402 <168461153+Avi0404@users.noreply.github.com>
2024-04-29 22:10:27 +0530

    Update fact1.c

--- fact1.c
+++ fact1.c
@@ -3,7 +3,7 @@ int main() {
     int n, j;
     unsigned long long fact = 1;
     printf("Enter an integer: ");
-    scanf("%d", &n);
+    scanf("%d",&n);
 
     // shows error if the user enters a negative integer
     if (n < 0)
